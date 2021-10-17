f6bb38da311e266e336bf5fbaf13f60573a9a0a0
Smovg-by <Smovg-by@users.noreply.github.com>
2021-10-17 13:33:27 +0300

    finish: add PORT variable

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const server = http.createServer((req, res) => {
 
   let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
   const ext = path.extname(filePath)
+  // зададим дефолтный content type
+  let contentType = "text.html"
+  // переключаем тип content type В зависимости от переменной
+    // и прокинем переменную contentType ниже в
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css'
+      break
+    case ".js":
+      contentType = "text/javascript"
+      break
+    default:
+      contentType = 'text/html'
+  }
 
   if (!ext) {
     filePath += '.html'
@@ -26,15 +40,19 @@ const server = http.createServer((req, res) => {
       })
     } else {
       res.writeHead(200, {
-        'Content-type': 'text-html'
+        // передаем сюда переменную, тобы в хедер передавался нужный MIME content type
+        'Content-type': contentType
       })
       res.end(content)
     }
   })
 })
 
+// прочитаем порт, если нет задаем по умолчанию 3000
+const PORT = process.env.PORT || 3000
+
 // запускаем прослушивание сервера на порт 3000
 // после запуска файла, можно открыть в браузере порт 3000 и просмотреть а нетворке preview
-server.listen(3000, () => {
-  console.log('Server has been started...')
+server.listen(PORT, () => {
+  console.log(`Server has been started on ${PORT}...`)
 })
