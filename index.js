b46e9843d64301423803ec45a22f7936e1190907
Smovg-by <Smovg-by@users.noreply.github.com>
2021-10-17 13:16:33 +0300

    refactor multipage server and add 404 page for not existing url

--- index.js
+++ index.js
@@ -4,29 +4,33 @@ const path = require('path')
 
 const server = http.createServer((req, res) => {
 
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-      if (err) {
-        throw err
-      }
-      res.writeHead(200, {
-        'Content-type': 'text/html' // хром парсит html
-        // 'Content-type': 'text/plain' // хром парсит обычный текст
-      })
-      res.end(data) // здесь то, чем завершится ответ от сервера
-    })
-  } else if (req.url === '/contact') {
-    fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
-      if (err) {
-        throw err
-      }
-      res.writeHead(200, {
-        'Content-type': 'text/html' // хром парсит html
-        // 'Content-type': 'text/plain' // хром парсит обычный текст
-      })
-      res.end(data) // здесь то, чем завершится ответ от сервера
-    })
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+  const ext = path.extname(filePath)
+
+  if (!ext) {
+    filePath += '.html'
   }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+        if (err) {
+          res.writeHead(500)
+          res.end('Error')
+        } else {
+          res.writeHead(200, {
+            'Content-type': 'text-html'
+          })
+          res.end(data)
+        }
+      })
+    } else {
+      res.writeHead(200, {
+        'Content-type': 'text-html'
+      })
+      res.end(content)
+    }
+  })
 })
 
 // запускаем прослушивание сервера на порт 3000
